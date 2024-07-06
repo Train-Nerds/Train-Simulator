f727a40812f2fe0c8a7e6a588dc54bfc9921fe07
Ethan Yntema <130712759+toastyntema@users.noreply.github.com>
2024-07-06 16:01:29 -0400

    Merged reading and writing
    Okay so I should've done it this way at first my bad

--- Train Simulation/TileMap.gd
+++ Train Simulation/TileMap.gd
@@ -10,15 +10,27 @@ var minimum = -500000
 var maximum = 500000
 @onready var heightmap = Image.create(width, height, false, Image.FORMAT_RGBA8)
 @onready var player = $"../player"
-@onready var tile_pos = player.position
+@onready var tile_pos = local_to_map(position)
+@onready var map = $noise_map.texture.get_image()
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#signal testing, should communicate with noise_read.gd
-	
-	
+	print(width, height)
+	map.convert(Image.FORMAT_RGBA8)
+	print(map.get_pixel(1,1))
+	for x in range(height):
+		for y in range(width):
+			##format is RGBA
+			write_r(map.get_pixel(x,y)[0], x, y)
+			write_g(map.get_pixel(x,y)[1], x, y)
+			write_b(map.get_pixel(x,y)[2], x, y)
+			write_a(map.get_pixel(x,y)[3], x, y)
+			
+			
+			
 	altitude.seed = randi()
 	##generate_chunk(player.position)
 	
@@ -30,6 +42,22 @@ func _ready():
 	else:
 		print("Ahhhhhhh, the image is burning!")
 	
+func write_r(red, x, y):
+		if red > 0:
+			set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2i(round(red/63.75),round(red/63.75)))
+
+func write_g(green, x, y):
+	if green > 0:
+		set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2i(2,0))
+	
+func write_b(blue, x, y):
+	
+	if blue > 0:
+		set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2i(3,2))
+func write_a(alpha, x, y):
+	if alpha < 1:
+		pass
+
 
 #signal testing
 
@@ -58,24 +86,13 @@ func _ready():
 	#
 	#print("Max: " + str(maximum) + ", Min: " + str(minimum))
 	#
+#
+#func _on_procedural_generation_red(red, x, y):
+	#var tile_pos = local_to_map(position)
+	#if red > 0:
+		#set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2i(round(red/63.75),round(red/63.75)))
+#
+#
 
-func _on_procedural_generation_red(red, x, y):
-	var tile_pos = local_to_map(position)
-	if red > 0:
-		set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2i(round(red/63.75),round(red/63.75)))
-
-
-func _on_procedural_generation_green(green, x, y):
-	var tile_pos = local_to_map(position)
-	if green > 0:
-		set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2i(2,0))
-	
-func _on_procedural_generation_blue(blue, x, y):
-	var tile_pos = local_to_map(position)
-	if blue > 0:
-		set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2i(3,2))
-func _on_procedural_generation_alpha(alpha, x, y):
-	if alpha > 1:
-		pass
 
 
--- Train Simulation/procedural_generation.gd
+++ Train Simulation/procedural_generation.gd
@@ -1,45 +1,44 @@
-@tool
 extends Node2D
-##tilemap reading
-
-@onready var map = $noise_map.texture.get_image()
-@onready var width = map.get_width()
-@onready var height = map.get_height()
-##@onready var player = get_parent().get_child(1)
-
-signal red
-signal green
-signal blue
-signal alpha
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print(width, height)
-	map.convert(Image.FORMAT_RGBA8)
-	print(map.get_pixel(1,1))
-	for x in range(height):
-		for y in range(width):
-			##format is RGBA
-			var red_color = map.get_pixel(x,y)[0]
-			var green_color = map.get_pixel(x,y)[1]
-			var blue_color = map.get_pixel(x,y)[2]
-			var alpha_color = map.get_pixel(x,y)[3]
-
-
-			emit_signal("red", red_color*255, x, y)
-			emit_signal("green", green_color*255, x, y)
-			emit_signal("blue", blue_color*255, x, y)
-			emit_signal("alpha", alpha_color*255, x, y)
-
-			#print(pixel_color * 255)
-
-	##generate_chunk(player.position)
-	
-	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func generate_chunk(position):
-	pass
+###tilemap reading
+#
+#@onready var map = $noise_map.texture.get_image()
+#@onready var width = map.get_width()
+#@onready var height = map.get_height()
+###@onready var player = get_parent().get_child(1)
+#
+#signal red
+#signal green
+#signal blue
+#signal alpha
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+	#print(width, height)
+	#map.convert(Image.FORMAT_RGBA8)
+	#print(map.get_pixel(1,1))
+	#for x in range(height):
+		#for y in range(width):
+			###format is RGBA
+			#var red_color = map.get_pixel(x,y)[0]
+			#var green_color = map.get_pixel(x,y)[1]
+			#var blue_color = map.get_pixel(x,y)[2]
+			#var alpha_color = map.get_pixel(x,y)[3]
+#
+#
+			#emit_signal("red", red_color*255, x, y)
+			#emit_signal("green", green_color*255, x, y)
+			#emit_signal("blue", blue_color*255, x, y)
+			#emit_signal("alpha", alpha_color*255, x, y)
+#
+			##print(pixel_color * 255)
+#
+	###generate_chunk(player.position)
+	#
+	#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#pass
+#
+#func generate_chunk(position):
+	#pass
