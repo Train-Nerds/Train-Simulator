c087aff0900aa9e971b84a706de35817aafffd6c
Ethan Yntema <130712759+toastyntema@users.noreply.github.com>
2024-07-06 10:40:24 -0400

    Signals
    -Move node tree around, renamed some duplicate variables

--- Train Simulation/TileMap.gd
+++ Train Simulation/TileMap.gd
@@ -6,11 +6,10 @@ var moisture = FastNoiseLite.new()
 ##set these for size
 var width = 1000
 var height = 1000
-##@onready var player = get_parent().get_child(1)
 var minimum = -500000
 var maximum = 500000
 @onready var heightmap = Image.create(width, height, false, Image.FORMAT_RGBA8)
-@onready var player = $ProceduralGeneration/player
+@onready var player = $"../player"
 @onready var tile_pos = player.position
 
 
@@ -37,28 +36,28 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
-
-func generate_chunk(position):
-	var tile_pos = local_to_map(position)
-	for x in range(width):
-		for y in range(height):
-			var alt = altitude.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
-			heightmap.set_pixel(x, y, Color(round((alt+10)/5), 0, 0, 255))
-			
-			if(alt > maximum):
-				maximum = alt
-			elif (alt < minimum):
-				minimum = alt
-			var temp = temperature.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
-			var moist = moisture.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
-#			set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2i(1,1))
-			if alt < 2:
-					set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2(0, round((moist+10)/5)))
-			else:
-					set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2(round((alt+10)/5), round((alt+10)/5)))
-	
-	print("Max: " + str(maximum) + ", Min: " + str(minimum))
-	
+#
+#func generate_chunk(position):
+	#var tile_pos = local_to_map(position)
+	#for x in range(width):
+		#for y in range(height):
+			#var alt = altitude.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
+			#heightmap.set_pixel(x, y, Color(round((alt+10)/5), 0, 0, 255))
+			#
+			#if(alt > maximum):
+				#maximum = alt
+			#elif (alt < minimum):
+				#minimum = alt
+			#var temp = temperature.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
+			#var moist = moisture.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
+##			set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2i(1,1))
+			#if alt < 2:
+					#set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2(0, round((moist+10)/5)))
+			#else:
+					#set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2(round((alt+10)/5), round((alt+10)/5)))
+	#
+	#print("Max: " + str(maximum) + ", Min: " + str(minimum))
+	#
 
 func _on_procedural_generation_red(red, x, y):
 	var tile_pos = local_to_map(position)
@@ -69,7 +68,7 @@ func _on_procedural_generation_green(green, x, y):
 	var tile_pos = local_to_map(position)
 	if green > 0:
 		set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2i(2,0))
-    
+	
 func _on_procedural_generation_blue(blue, x, y):
 	var tile_pos = local_to_map(position)
 	if blue > 0:
--- Train Simulation/procedural_generation.gd
+++ Train Simulation/procedural_generation.gd
@@ -14,22 +14,21 @@ signal alpha
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	print(width, height)
 	map.convert(Image.FORMAT_RGBA8)
 	print(map.get_pixel(1,1))
 	for x in range(height):
 		for y in range(width):
 			##format is RGBA
-			var red = map.get_pixel(x,y)[0]
-			var green = map.get_pixel(x,y)[1]
-			var blue = map.get_pixel(x,y)[2]
-			var alpha = map.get_pixel(x,y)[3]
+			var red_color = map.get_pixel(x,y)[0]
+			var green_color = map.get_pixel(x,y)[1]
+			var blue_color = map.get_pixel(x,y)[2]
+			var alpha_color = map.get_pixel(x,y)[3]
 
-			emit_signal("red", red*255, x, y)
-			emit_signal("green", green*255, x, y)
-			emit_signal("blue", blue*255, x, y)
-			emit_signal("alpha", alpha*255, x, y)
+			emit_signal("red", red_color*255, x, y)
+			emit_signal("green", green_color*255, x, y)
+			emit_signal("blue", blue_color*255, x, y)
+			emit_signal("alpha", alpha_color*255, x, y)
 			#print(pixel_color * 255)
 
 	##generate_chunk(player.position)
