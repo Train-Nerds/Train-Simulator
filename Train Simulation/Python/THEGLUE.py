e79c32a344f6b11b3bdb5eec84d37b175e1bf4a9
Profetes <bbsaw1108@gmail.com>
2024-07-07 20:17:00 -0400

    SAVE ME, GOD
    FOR I HAVE BEEN ABANDONED BY THEE O GITHUB

--- Train Simulation/Python/THEGLUE.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-import os
-
-print(os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + "\\TrainerdsBuilt.exe")
-os.system("\"" + os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + "\\TrainerdsBuilt.console.exe\"")
